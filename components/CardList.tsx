4396438fb75be20566bd9a6732d10ebc191959f4
sam <s.ojling@gmail.com>
2021-03-26 17:51:41 -0700

    Created Select component and filter handler for filtering the data

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,46 +1,50 @@
 import { useState } from "react";
-import {
-  filterDataBy,
-  height,
-  id,
-  costPerLaunch,
-  numberOfLandingLegs,
-} from "../utils/filters";
+import Card from "./Card";
+import Select from "./Select";
+import { displayDate } from "../utils/date";
+import { rocketFilters, filterDataBy } from "../utils/filters";
 
 const CardList = ({ rockets }) => {
+  const [filter, setFilter] = useState(null);
   const [filteredRockets, setFilteredRockets] = useState(rockets);
 
-  const handleFilter = (filter) => {
-    const result = filterDataBy(filter, rockets);
+  const handleFilter = (event) => {
+    const { filterFunc } = rocketFilters[event.target.value];
+
+    setFilter(event.target.value);
+    const result = filterDataBy(filterFunc, rockets);
     setFilteredRockets([...result]);
   };
 
   return (
     <>
-      <div className="flex">
-        <div className="m-5">
-          <button onClick={() => handleFilter(height)}>height</button>
-        </div>
-        <div className="m-5">
-          <button onClick={() => handleFilter(id)}>id</button>
-        </div>
-        <div className="m-5">
-          <button onClick={() => handleFilter(costPerLaunch)}>cost</button>
-        </div>
-        <div className="m-5">
-          <button onClick={() => handleFilter(numberOfLandingLegs)}>
-            legs
-          </button>
-        </div>
+      <div className="max-w-4xl m-auto">
+        <Select
+          className="mb-4 block"
+          label="Sort by"
+          value={filter}
+          onChange={handleFilter}
+        >
+          {Object.entries(rocketFilters).map(([filterName, values]) => (
+            <option key={filterName} value={filterName}>
+              {values.label}
+            </option>
+          ))}
+        </Select>
+        {filteredRockets.map((rocket) => (
+          <div key={rocket.id} className="mb-10">
+            <Card
+              name={rocket.name}
+              description={rocket.description}
+              metaData={[
+                `${rocket.height.meters} meters`,
+                `${rocket.landing_legs.number} landing legs`,
+                `First flight ${displayDate(rocket.first_flight)}`,
+              ]}
+            />
+          </div>
+        ))}
       </div>
-      {filteredRockets.map((rocket) => (
-        <div key={rocket.id} className="mb-10">
-          <p>ID: {rocket.id}</p>
-          <p>Height: {rocket.height.meters}</p>
-          <p>Cost: {rocket.cost_per_launch}</p>
-          <p>Legs: {rocket.landing_legs.number}</p>
-        </div>
-      ))}
     </>
   );
 };
