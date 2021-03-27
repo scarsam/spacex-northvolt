f083a29de7201d8f5fa76eeb916f4cd60720c6b2
sam <s.ojling@gmail.com>
2021-03-26 17:51:07 -0700

    Replaced the card with actual data

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,24 +1,18 @@
 import Pill from "./Pill";
 
-const Card = () => {
+const Card = ({ name, description, metaData }) => {
   return (
-    <div className="bg-white rounded-md text-black flex max-w-4xl m-auto">
+    <div className="bg-white rounded-md text-black flex">
       <div className="bg-black rounded-md py-16 px-32 m-4">
         <img src="/images/rockets/rocket-1.svg" />
       </div>
       <div className="content flex-1 text-left p-10">
-        <h2 className="text-2xl font-semibold pb-2">Falcon 1</h2>
-        <p>
-          Description: The Falcon 1 was an expendable launch system privately
-          developed and manufactured by SpaceX during 2006-2009. On 28 September
-          2008, Falcon 1 became the first privately-developed liquid-fuel launch
-          vehicle to go into orbit around the Earth.
-        </p>
-        {/* Pill Component */}
+        <h2 className="text-2xl font-semibold pb-2">{name}</h2>
+        <p>{description}</p>
         <div className="flex pt-4">
-          <Pill data="32.25m" />
-          <Pill data="2 landing legs" />
-          <Pill data="First flight 2006-03-04" />
+          {metaData.map((data) => (
+            <Pill data={data} />
+          ))}
         </div>
       </div>
     </div>
