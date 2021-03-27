2d391b5905ac3e97e1e03c043e7d2b1802bd82f2
sam <s.ojling@gmail.com>
2021-03-26 18:39:05 -0700

    Tweaked container size, added Nortvolt favicon and Logo to page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      screens: {
+        "2xl": "1200px",
+      },
+    },
     extend: {
       colors: {
         "space-dark": "#0C090A",
